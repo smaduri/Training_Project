29ca6fa165dcb961b1b5392820e5241c36345ca2
machzqcq <pradeep@seleniumframework.com>
2014-09-27 17:22:30 -0400

    converted the hash to ruby 1.9 syntax

--- features/step_definitions/ruby_enumerable.rb
+++ features/step_definitions/ruby_enumerable.rb
@@ -1,7 +1,7 @@
 And(/^I click view details for "([^"]*)"$/) do |arg|
-  puppy_names = @browser.find_elements(:class =>'name')
-  index_hanna = puppy_names.find_index {|puppy| puppy.text == arg}
-  @browser.find_elements(:class => 'rounded_button')[index_hanna].click
+  puppy_names = @browser.find_elements(class: 'name')
+  index_hanna = puppy_names.find_index { |puppy| puppy.text == arg }
+  @browser.find_elements(class: 'rounded_button')[index_hanna].click
 
 end
 
