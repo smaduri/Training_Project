2ab28f95340dbdf3f2c8895db312aa42b824e2a0
machzqcq <pradeep@seleniumframework.com>
2014-10-12 07:44:09 -0400

    selenium grid example

---
+++ features/Scripts/seleniumgrid_example.rb
@@ -0,0 +1,10 @@
+# require 'selenium-webdriver'
+#
+# @browser = Selenium::WebDriver.for(:remote, :url => "http://localhost:4444/wd/hub",:desired_capabilities => :chrome)
+#
+#
+# @browser.navigate.to "http://www.google.com"
+#
+# p @browser.title
+#
+# @browser.quit
