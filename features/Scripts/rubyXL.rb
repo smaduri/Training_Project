7931d7b20aa325abdb66cc975658e4a8c7132c7f
machzqcq <pradeep@seleniumframework.com>
2014-10-16 21:05:32 -0400

    simple excel script

---
+++ features/Scripts/rubyXL.rb
@@ -0,0 +1,39 @@
+require 'rubyXL'
+#
+# workbook = RubyXL::Workbook.new
+#
+# worksheet = workbook.add_worksheet('Sheet2')
+# worksheet.sheet_name = 'Cool New Name'
+#
+# worksheet.add_cell(0, 0, 'firstname')
+#
+# workbook.write("./myworkbook.xlsx")
+
+# workbook = RubyXL::Parser.parse('./myworkbook.xlsx')
+#
+# worksheet = workbook['Cool New Name']
+#
+# data = worksheet.extract_data
+#
+# puts "current value: #{data[0][0]}"
+#
+# worksheet[0][0].change_contents("firstname")
+#
+# workbook.write("./myworkbook.xlsx")
+#
+# workbook = RubyXL::Parser.parse('./myworkbook.xlsx')
+#
+# worksheet = workbook['Cool New Name']
+#
+# data = worksheet.extract_data
+#
+# puts "changed value: #{data[0][0]}"
+#
+# worksheet.delete_cell(0,0)
+#
+# workbook.write("./myworkbook.xlsx")
+
+
+
+
+
