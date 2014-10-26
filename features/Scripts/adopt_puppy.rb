bafaae343aefd858682f7e32d634973c71de8612
machzqcq <pradeep@seleniumframework.com>
2014-10-26 11:27:45 -0400

    ruby module example

---
+++ features/Scripts/adopt_puppy.rb
@@ -0,0 +1,7 @@
+# require_relative './simple_module.rb'
+#
+# include Keywords
+#
+# create_browser
+# adopt_puppy
+# close_browser
---
+++ features/Scripts/simple_module.rb
@@ -0,0 +1,23 @@
+# require 'watir-webdriver'
+# module Keywords
+#
+#
+# def create_browser
+#   @browser = Watir::Browser.new :chrome
+# end
+#
+#
+# def adopt_puppy
+#   @browser.goto 'http://puppies.herokuapp.com'
+#   @browser.button(class: 'rounded_button').click
+#   @browser.button(class: 'rounded_button').click
+#   @browser.button(class: 'rounded_button').click
+# end
+#
+#
+# def close_browser
+#   @browser.quit
+# end
+#
+#
+# end
